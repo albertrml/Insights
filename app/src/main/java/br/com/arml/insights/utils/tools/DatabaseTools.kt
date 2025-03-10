8bae3e5569d10fc7a51b2449d93c115c49d91681
arml <albertrml.dev@gmail.com>
2025-03-10 14:54:32 -0300

    Adding TagRepositoryTest

--- app/src/main/java/br/com/arml/insights/utils/tools/DatabaseTools.kt
+++ app/src/main/java/br/com/arml/insights/utils/tools/DatabaseTools.kt
@@ -1,20 +1,6 @@
 package br.com.arml.insights.utils.tools
 
 import br.com.arml.insights.utils.data.Response
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-
-/*fun <T> performDatabaseOperation(
-    databaseOperation: suspend () -> T
-): Flow<Response<T>> = flow {
-    emit(Response.Loading)
-    try {
-        emit(Response.Success(databaseOperation()))
-    }
-    catch (e: Exception) {
-        emit(Response.Failure(e))
-    }
-}*/
 
 suspend fun <T> performDatabaseOperation(
     databaseOperation: suspend () -> T
